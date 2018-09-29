3f9c4c45551af051104d33de4b62ada296f1abb1
Sander ten Brinke <sander.ten.brinke@xs4all.nl>
2018-09-29 14:35:42 +0200

    Completed Exercise 3

--- AND101-master/Selfmade/TipCalculator/MainActivity.cs
+++ AND101-master/Selfmade/TipCalculator/MainActivity.cs
@@ -4,17 +4,53 @@ using Android.OS;
 
 namespace TipCalculator
 {
-	[Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private EditText _inputBill;
+        private Button _calculateButton;
+        private TextView _outputTip;
+        private TextView _outputTotal;
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-		}
-	}
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            GetViews();
+
+            _calculateButton.Click += OnCalculateClick;
+        }
+
+        private void OnCalculateClick(object sender, System.EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(_inputBill.Text) || !double.TryParse(_inputBill.Text, out double bill))
+            {
+                Toast
+                    .MakeText(this, "The bill is not valid input", ToastLength.Short)
+                    .Show();
+                return;
+            }
+
+            var tip = bill * 0.15;
+            var total = bill + tip;
+
+            _outputTip.Text = tip.ToString();
+            _outputTotal.Text = total.ToString();
+            
+        }
+
+
+        private void GetViews()
+        {
+            _inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            _calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            _outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            _outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+        }
+    }
 }
 
 
